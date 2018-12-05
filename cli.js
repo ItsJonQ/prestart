95109471e7781aff063a24663cef80bba4111d6e
Jon Q <hello@jonquach.com>
2018-12-04 19:47:16 -0500

    Add index and cli files

---
+++ cli.js
@@ -0,0 +1,6 @@
+#!/usr/bin/env node
+"use strict";
+
+const preStart = require("./index");
+
+preStart.sync();
--- index.js
+++ index.js
@@ -1,35 +1,50 @@
-#!/usr/bin/env node
 "use strict";
 
 const checkDependencies = require("check-dependencies");
 const isOnline = require("is-online");
 
-console.log("🔑 ", "Prestart");
-console.log("");
+function Prestart() {
+  console.log("🔑 ", "PreStart");
+  console.log("");
 
-console.log("  ", "🔌 ", "Check connection");
+  console.log("  ", "🔌 ", "Check connection");
 
-const tasks = new Promise(resolve => {
-  return isOnline({ timeout: 2500 }).then(online => {
-    if (!online) {
-      console.log("  ", "📦 ", "Install dependencies (Skipped)");
+  const tasks = new Promise(resolve => {
+    return isOnline({ timeout: 2500 }).then(online => {
+      if (!online) {
+        console.log("  ", "📦 ", "Install dependencies (Skipped)");
+        console.log("");
+        return resolve();
+      }
+      console.log("  ", "📦 ", "Install dependencies");
       console.log("");
-      return resolve();
-    }
-    console.log("  ", "📦 ", "Install dependencies");
-    console.log("");
-    const options = {
-      checkGitUrls: true,
-      install: true,
-      packageManager: "npm",
-      verbose: false
-    };
-    return checkDependencies(options).then(() => {
-      resolve();
+      const options = {
+        checkGitUrls: true,
+        install: true,
+        packageManager: "npm",
+        verbose: false
+      };
+      return checkDependencies(options).then(() => {
+        resolve();
+      });
     });
   });
-});
 
-tasks.then(() => {
-  console.log("✨ ", "Have fun developing!");
-});
+  tasks.then(() => {
+    console.log("✨ ", "Have fun developing!");
+  });
+
+  return tasks;
+}
+
+function sync(cb) {
+  new Prestart().then(cb).catch(cb);
+}
+
+function prestart() {
+  return new Prestart();
+}
+
+prestart.sync = sync;
+
+module.exports = prestart;
